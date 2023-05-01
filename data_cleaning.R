767ad6dbeff1952f68bb666ed052b939d07e48dd
kashacepok2 <kasha.cepok@googlemail.com>
2023-05-01 13:32:27 +0100

    grouped rating, binded 2017+ data

--- data_cleaning.R
+++ data_cleaning.R
@@ -14,6 +14,20 @@ other <- c("TG16", "PCFX", "2600", "3D0", "WS", "NG")
 
 video_game_sales <- read_csv("data/extra_videogame_sales.csv") %>% 
   clean_names()
+
+video_game_sales_original <- read_csv("data/sales-2019.csv") %>% 
+  clean_names()
+
+video_game_sales_after_2017 <- video_game_sales_original %>%
+  select(-rank, -basename, -vg_chartz_score, -critic_score, -user_score, -pal_sales,
+         -last_update, -status, -vgchartzscore, -url, -img_url) %>% 
+  rename("year_of_release" = "year") %>% 
+  rename("rating" = "esrb_rating") %>% 
+  filter(year_of_release > 2016) %>% 
+  filter(is.na(global_sales) == FALSE
+         & is.na(na_sales) == FALSE
+         & is.na(jp_sales) == FALSE
+         & is.na(other_sales) == FALSE)
   
 video_game_ratings <- video_game_sales %>% 
   filter(user_count > 25) %>% 
@@ -30,11 +44,21 @@ video_game_ratings <- video_game_sales %>%
   )) %>% 
   pivot_longer(cols = c("critic_score", "user_score"),
                names_to = "critic_or_user", values_to = "score") %>% 
-  filter(is.na(genre) == FALSE | year_of_release < 2017)
+  filter(is.na(genre) == FALSE | year_of_release < 2017) %>% 
+  mutate(rating = case_when(
+    rating == "E" ~ "All ages",
+    rating == "EC" ~ "3+",
+    rating == "E10+" ~ "10+",
+    rating == "T" ~ "13+",
+    rating == "M" ~ "17+",
+    rating == "AO" ~ "18+",
+    rating == "RP" ~ "RP"
+  ))
 
 video_game_sales_clean <- video_game_sales %>% 
   select(-user_count, -user_score, -critic_score, -critic_count) %>% 
-  mutate(year_of_release = as.numeric(year_of_release)) %>% 
+  mutate(year_of_release = as.numeric(year_of_release)) %>%
+  bind_rows(video_game_sales_after_2017) %>% 
   mutate(platform = case_when(
     platform %in% nintendo ~ "Nintendo",
     platform %in% xbox ~ "Microsoft Xbox",
@@ -43,7 +67,17 @@ video_game_sales_clean <- video_game_sales %>%
     platform %in% other ~ "Other",
     platform == "PC" ~ "PC"
   )) %>% 
-  filter(is.na(genre) == FALSE | year_of_release < 2017) 
+  filter(is.na(genre) == FALSE) %>%
+  filter(year_of_release != 2020) %>% 
+  mutate(rating = case_when(
+    rating == "E" ~ "All ages",
+    rating == "EC" ~ "3+",
+    rating == "E10+" ~ "10+",
+    rating == "T" ~ "13+",
+    rating == "M" ~ "17+",
+    rating == "AO" ~ "18+",
+    rating == "RP" ~ "RP"
+  ))
 
 write.csv(video_game_sales_clean, "vg_sales.csv")
 write.csv(video_game_ratings, "vg_ratings.csv")
